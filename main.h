ec55297949f94c8f5c83a153746ef315a504e262
Werku Birhanu Amsal <workzman12@gmail.com>
2022-07-30 03:06:17 -0700

    Header

--- main.h
+++ main.h
@@ -1,44 +1,53 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+#include <unistd.h> 
 #include <stdarg.h>
 
-/**
-* struct convert - defines a structure for symbols and functions
-* @sym: The operator
-* @f: The function associated
-*/
-struct convert
-{
-	char *sym;
-	int (*f)(va_list);
-};
-typedef struct convert conver_t;
-
-/*Main functions*/
-int parser(const char *format, conver_t f_list[], va_list arg_list);
 int _printf(const char *format, ...);
-int _write_char(char);
-int print_char(va_list);
-int print_string(va_list);
-int print_percent(va_list);
-int print_integer(va_list);
-int print_number(va_list);
-int print_binary(va_list);
-int print_reversed(va_list arg);
-int rot13(va_list);
-int unsigned_integer(va_list);
-int print_octal(va_list list);
-int print_hex(va_list list);
-int print_heX(va_list list);
+int print_char(va_list ap);
+int print_str(va_list ap);
+int print_nbr(va_list ap);
+int print_binary(va_list ap);
+int print_octal(va_list ap);
+int print_hexa_lower(va_list ap);
+int print_hexa_upper(va_list ap);
+int print_unsigned(va_list ap);
+int print_str_unprintable(va_list ap);
+int print_str_reverse(va_list ap);
+int print_ptr(va_list ap);
+int print_rot13(va_list ap);
+int print_percent(va_list ap __attribute__((unused)));
+int print_number(int n);
+int print_unsigned_number(unsigned int n);
+int print_o(va_list o);
+int print_b(va_list b);
+int print_u(va_list u);
+int _putchar(char c);
+int _puts(char *str, int ascii);
+int _nbr_len(int prmNumber);
+int print_buf(char *buf, unsigned int nbuf);
+int (*get_op(const char c))(va_list);
+int _strlen_recursion(char *s);
+int convert_alpha_numeric(int nb, int upper);
+char *convert_rot13(char *str);
+char *convert_base(unsigned long nb, unsigned int base, int upper);
+char *_strdup(char *str);
+char *convert_base_pointer(unsigned long p);
+char *_itoa(int prmNumber);
 
-/*Helper functions*/
-unsigned int base_len(unsigned int, int);
-char *rev_string(char *);
-void write_base(char *str);
-char *_memcpy(char *dest, char *src, unsigned int n);
-int print_unsgined_number(unsigned int);
+/**
+ * struct flags_printf - struct conversion to function
+ * @c: flag string
+ * @f: pointer to func
+ */
 
+typedef struct flags_printf
+{
+	char *c;
+	int (*f)(va_list);
+} flags_p;
 #endif
